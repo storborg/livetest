b6079e6c89ce33530ef91589622fcd829eb13dfc
Scott Torborg <scott@crookedmedia.com>
2009-10-31 21:05:53 -1000

    make a proper doctest suite

--- livetest/__init__.py
+++ livetest/__init__.py
@@ -3,36 +3,32 @@ LiveTest - Like WebTest, but on a live site.
 
 Setup an app to test against with just a hostname:
 
-    >>> import livetest
-    >>> app = livetest.TestApp('www.google.com')
+>>> import livetest
+>>> app = livetest.TestApp('www.google.com')
 
 Make requests just like WebTest:
 
-    >>> resp = app.get('/')
+>>> resp = app.get('/')
 
 Grab forms:
 
-    >>> resp.forms
-    {0: <webtest.Form object at 0x10118ac50>}
-    >>> form = resp.forms[0]
-    >>> form.fields
-    {'btnI': [<webtest.Submit object at 0x10118ae10>], 'btnG': [<webtest.Submit
-    object at 0x10118add0>], 'q': [<webtest.Text object at 0x10118ad90>],
-    'source': [<webtest.Hidden object at 0x10118ad10>], 'hl': [<webtest.Hidden
-    object at 0x10118acd0>], 'ie': [<webtest.Hidden object at 0x10118ad50>]}
+>>> resp.forms # doctest: +ELLIPSIS
+{0: <webtest.Form object at 0x...>}
+>>> form = resp.forms[0]
+>>> form.fields # doctest: +ELLIPSIS
+{'btnI': [<webtest.Submit object at 0x...>], 'btnG': [<webtest.Submit object at 0x...>], 'q': [<webtest.Text object at 0x...>], 'source': [<webtest.Hidden object at 0x...>], 'hl': [<webtest.Hidden object at 0x...>], 'ie': [<webtest.Hidden object at 0x...>]}
 
 Submit forms:
 
-    >>> form['q'] = 'python testing'
-    >>> resp = form.submit()
+>>> form['q'] = 'python testing'
+>>> resp = form.submit()
 
 Test stuff in the response:
 
-    >>> resp.mustcontain('Agile', 'unittest', 'PyUnit')
-    >>> resp
-    <200 OK text/html body='<!doctype...v>  '/25498>
-    >>> resp.status
-    '200 OK'
+>>> resp.mustcontain('Agile', 'unittest', 'PyUnit')
+>>> resp.status
+'200 OK'
+
 """
 
 __author__ = 'scott@crookedmedia.com'
