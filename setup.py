2c46c2e33bac52708dc2feb7d18c511b84d6d2a7
Scott Torborg <scott@crookedmedia.com>
2009-11-01 15:14:24 -1000

    update homepage url so that it points to a tarball

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(name='livetest',
-      version='0.1dev',
+      version='0.2dev',
       description='Live WebTesting',
       long_description='Make live website tests look like Paste WebTest',
       classifiers=[
@@ -21,7 +21,7 @@ setup(name='livetest',
       keywords='wsgi test unit tests testing web functional',
       author='Scott Torborg',
       author_email='storborg@mit.edu',
-      url='http://github.com/storborg/livetest',
+      url='http://github.com/storborg/livetest/tarball/master',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'tests']),
       install_requires=['WebTest'],
