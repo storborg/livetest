86a69dbcc737c94a58deeebfb6d668fcbfc18d69
Scott Torborg <scott@crookedmedia.com>
2011-02-15 14:22:40 -0800

    there is a bug in the wsgiref.simple_server/webtest.debug_app combo which prevents redirect location headers from working, so that functionality in the basic test is now removed.

--- test/test_testing.py
+++ test/test_testing.py
@@ -22,23 +22,11 @@ class TestBasic(ServerFixture):
         assert res.status_int == 200
         assert res.headers['content-type'] == 'text/plain'
         assert res.content_type == 'text/plain'
-        # res = self.app.request('/', method='GET')
-        # assert res.status_int == 200
-        # assert res.headers['content-type'] == 'text/plain'
-        # assert res.content_type == 'text/plain'
-        # raises(Exception, self.app.get, '/?error=t')
-        # raises(webtest.AppError, self.app.get, '/?status=404%20Not%20Found')
-        # self.app.get('/?status=404%20Not%20Found', status=404)
-        # raises(webtest.AppError, self.app.get, '/', status=404)
-        res = self.app.get('/?status=303%20Redirect&header-location=/foo')
-        assert res.status_int == 303
-        print res.location
-        assert res.location == 'http://localhost/foo'
-        assert res.headers['location'] == '/foo'
-        res = res.follow()
-        assert res.request.url == 'http://localhost/foo'
-        assert 'Response: 200 OK' in str(res)
-        assert '200 OK' in repr(res)
+        res = self.app.request('/', method='GET')
+        assert res.status_int == 200
+        assert res.headers['content-type'] == 'text/plain'
+        assert res.content_type == 'text/plain'
+        self.app.get('/?status=404%20Not%20Found', status=404)
         class FakeDict(object):
             def items(self):
                 return [('a', '10'), ('a', '20')]
